a62e7aaa56cee3facda4f2b00d399158d72329d5
Anastasiyu <asy.safiulina@mail.ru>
2022-12-02 13:31:14 +0300

    Initial commit

---
+++ src/main/java/com/example/demoz/DemoZApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoZApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoZApplication.class, args);
+    }
+
+}
